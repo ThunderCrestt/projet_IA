ef8d1de4f168e6c1b5a3c3cbf2b18ad3ab059507
Mona LE COZ <m6lecoz@enib.fr>
2021-04-17 16:51:52 -0400

    Ajout de la classe Case et liaison avec environment

--- ProjetIA/ProjetIA/Environment.cs
+++ ProjetIA/ProjetIA/Environment.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace ProjetIA
 {
+
+
     class Environment
     {
+        private List<List<Case>> _grid;
+        public List<List<Case>> Grid
+        {
+            get { return _grid; }
+        }
+
+        public Environment()
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                List<Case> line = new List<Case> ();
+                for (int i = 0; i < 7; i++)
+                {
+                    Case y = new Case(i, j);
+                    line.Add(y);                    
+                }
+                _grid.Add(line);
+            }
+        }
+        ~Environment() { }
+
+        public void restartGame()
+        {
+            for (int j = 0; j < 6; j ++)
+            {
+                for (int i = 0; i < 7; i ++)
+                {
+                    _grid[i][j].State = caseState.empty;
+                }
+            }
+        }
+
+        public Case FoundCase(int i, int j)
+        {
+            return _grid[i][j];
+        }
+        
     }
 }

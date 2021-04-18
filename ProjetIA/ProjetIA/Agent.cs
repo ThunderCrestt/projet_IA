6ef560e5deb54a6133d80940b852c36582ac742f
Mona LE COZ <m6lecoz@enib.fr>
2021-04-18 16:22:16 -0400

    structure

---
+++ ProjetIA/ProjetIA/Agent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetIA
+{
+    class Agent
+    {
+        private struct Beliefs{
+            
+        }
+
+        Beliefs beliefs;
+        //Sensor
+        //Effector
+
+    }
+}

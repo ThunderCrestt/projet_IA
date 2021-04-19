0e4e53b666bcceae49297067613699b4e4a05320
vincentVBB <vincent.v2b@gmail.com>
2021-04-19 14:32:02 -0400

    effector and captor classes

--- ProjetIA/ProjetIA/Effector.cs
+++ ProjetIA/ProjetIA/Effector.cs
@@ -1,3 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace ProjetIA
 {
     class Effector
@@ -9,7 +15,7 @@ namespace ProjetIA
         }
 
         //Placer un pion aux coordonnées indiquées
-        public void putPawn(caseState pawn, int x, int y)
+        public void PutPawn(caseState pawn, int x, int y)
         {
             Environment.FoundCase(x, y).State = pawn;
         }
